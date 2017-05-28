0d4623299b1da6028235fb878c09109bb8bf2b2d
jarodhine <jarod.hine@my.jcu.edu.au>
2017-05-28 13:58:05 +1000

    Updated support page. Added placeholder php page.

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Refresh" content="0; url=signupthankyou.html" />
+<title>Untitled Document</title>
+</head>
+<p><a href="signupthankyou.html">Continue</a></p>
+<body>
+</body>
+</html>
